ad8ae268d45152c5a1e8f24a6d3db2975ae6e344
Guruprasad Lakshmi Narayanan <guruprasad.ln@canonical.com>
2024-02-15 16:42:11 +0530

    Fix the build error with snapcraft 8.x
    There was a bug in the sitecustomize.py file bundled with the lpci snap,
    which caused the build process to depend on the version of python used
    by snapcraft. This bug didn't surface with snapcraft 7.x, which is built
    on core20, the same as lpci snap. But snapcraft 8.x is built on core22
    and hence there was a conflict between the expected and available versions
    of pip during the build process.
    
    This fixes the issue by enabling the site customizations only in the
    context of the lpci snap.
    
    LP: #2053109

--- snap/local/sitecustomize.py
+++ snap/local/sitecustomize.py
@@ -1,8 +1,12 @@
 import os
 import site
 
+snap_name = os.getenv("SNAP_NAME")
 snap_dir = os.getenv("SNAP")
-if snap_dir:
+
+# Add the custom site directories only when executing in the context
+# of the `lpci` snap and its snap directory exists.
+if snap_name == "lpci" and snap_dir:
     site.ENABLE_USER_SITE = False
     site.addsitedir(os.path.join(snap_dir, "lib"))
     site.addsitedir(os.path.join(snap_dir, "usr/lib/python3/dist-packages"))
